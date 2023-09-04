b8e2f8d7e6b2366ceb5a9902ff455f79c6a83d3e
kartik wadile <kartikwadile@gmail.com>
2023-09-04 20:28:09 +0530

    resovled now

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-def addition(a,b,c):
-    print(a+b+c)
-=======
 ## addition code 
 def addition(a,b,c):
-    return a+b+c
->>>>>>> 27b4f4467cab0e4857ad985766680836a1aff1e6
+     print(a+b+c)
+     return a+b+c
 
 def multiplication(a,b):
     return a*b
